284cc52afe3fb0da625b6843739d6a2815947073
Gilles Pietri <gpietri@dawan.fr>
2017-09-18 12:07:55 +0200

    real usecase

--- windows_adobe.py
+++ windows_adobe.py
@@ -4,18 +4,11 @@ apps = { "After": r"Adobe After Effects CC 2015\Support Files\AfterFX.exe",
         "Bridge": r"Adobe Bridge CC (64 Bit)\Bridge.exe"
         }
 for app, path in apps.items():
-        print(app + " - " + root + path)
-
-npp = App(r'C:\Program Files\Notepad++\notepad++.exe')
-npp.open()
-wait(2)
-print(npp)
-wait("1505728633956.png")
-click("1505728633956.png")
-wait("1505728661330.png")
-click("1505728661330.png")
-npp.focus()
-type(10*'Hello NPP ')
-
-wait(3)
-npp.close()
+        print('Considering ' + app + '(' + root + path + ')')
+        sapp = App(root + path)
+        sapp.open()
+        wait(2)
+        wait("1505729222650.png")
+        click("1505729222650.png")
+        wait(2)
+        sapp.close()
