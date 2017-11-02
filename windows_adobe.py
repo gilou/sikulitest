c3cdc5d81101b5c15d90afa94b6a4e7001ee2981
Gilles Pietri <gpietri@dawan.fr>
2017-11-02 11:40:30 +0100

    Test Lightroom

--- windows_adobe.py
+++ windows_adobe.py
@@ -1,7 +1,8 @@
 root = 'C:\\Program Files\\Adobe\\'
 apps = { #"After": r"Adobe After Effects CC 2015\Support Files\AfterFX.exe",
-        "Audition": r"Adobe Audition CC 2015\Adobe Audition CC.exe"
-        #, "Bridge": r"Adobe Bridge CC (64 Bit)\Bridge.exe"
+        #"Audition": r"Adobe Audition CC 2015\Adobe Audition CC.exe"
+        #, "Bridge": r"Adobe Bridge CC (64 Bit)\Bridge.exe",
+        "LightRoom": r"Adobe Lightroom\lightroom.exe"
         }
 for app, path in apps.items():
         print('Considering ' + app + '(' + root + path + ')')
