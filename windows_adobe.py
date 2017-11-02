706c4b6af8ccc52d4190c095ab509a282d9ea3da
Gilles Pietri <gpietri@dawan.fr>
2017-11-02 11:35:59 +0100

    Stratégie différente : attendre l'acceptation de la licence directement, et passer sur la valid de l'éval sinon

--- windows_adobe.py
+++ windows_adobe.py
@@ -3,19 +3,17 @@ apps = { #"After": r"Adobe After Effects CC 2015\Support Files\AfterFX.exe",
         "Audition": r"Adobe Audition CC 2015\Adobe Audition CC.exe"
         #, "Bridge": r"Adobe Bridge CC (64 Bit)\Bridge.exe"
         }
-setFindFailedResponse(SKIP)
 for app, path in apps.items():
         print('Considering ' + app + '(' + root + path + ')')
         sapp = App(root + path)
         sapp.open()
         try:
+            wait("1509618378336.png", 15)
+            click("1509618378336.png")
+            wait(2)
+        except FindFailed:
             wait("1505729222650.png", 35)
             click("1505729222650.png") 
             wait("1509617897369.png", 5)
             click("1509617897369.png")
-            wait("1509618378336.png", 35)
-            click("1509618378336.png")
-            wait(2)
-        except FindFailed:
-            print('Failed')
             wait(2)
