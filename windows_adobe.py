1c5f19f1118e142149a9b2f9ed134c9205422ccb
Gilles Pietri <gpietri@dawan.fr>
2017-11-02 12:07:15 +0100

    infos debug

--- windows_adobe.py
+++ windows_adobe.py
@@ -6,7 +6,9 @@ apps = { #"After": r"Adobe After Effects CC 2015\Support Files\AfterFX.exe",
         "Premiere": r"Adobe Premiere Pro CC 2015\Adobe Premiere Pro.exe"
         }
 def accept():
+    print('En attente du bouton accepter pour 15s...')
     wait("1509618378336.png", 15)
+    print('OK, clic accepter')
     click("1509618378336.png")         
 
 for app, path in apps.items():
@@ -16,9 +18,12 @@ for app, path in apps.items():
         try:
             accept()            
         except FindFailed:
+            print('Echec de la première tentative, attente fenêtre')
             wait("1505729222650.png", 35)
             click("1505729222650.png") 
+            print('Attente Eval')
             wait("1509617897369.png", 15)
             click("1509617897369.png")
+            print('OK')
             wait(2)
             accept()
