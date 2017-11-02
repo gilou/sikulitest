9dc4cfc34a813911f4011080f5d9b6fc25c7f6b9
Gilles Pietri <gpietri@dawan.fr>
2017-11-02 11:57:18 +0100

    Quick & dirty script pour lancer sikuli

---
+++ deploy.cmd
@@ -0,0 +1,3 @@
+cd C:\temp
+git clone https://github.com/gilou/sikulitest.git windows_adobe.sikuli
+C:\Users\Stagiaire\Downloads\runsikulix.cmd -r C:\temp\windows_adobe.sikuli
--- windows_adobe.py
+++ windows_adobe.py
@@ -4,17 +4,20 @@ apps = { #"After": r"Adobe After Effects CC 2015\Support Files\AfterFX.exe",
         #, "Bridge": r"Adobe Bridge CC (64 Bit)\Bridge.exe",
         "LightRoom": r"Adobe Lightroom\lightroom.exe"
         }
+def accept():
+    wait("1509618378336.png", 15)
+    click("1509618378336.png")         
+
 for app, path in apps.items():
         print('Considering ' + app + '(' + root + path + ')')
         sapp = App(root + path)
         sapp.open()
         try:
-            wait("1509618378336.png", 15)
-            click("1509618378336.png")
-            wait(2)
+            accept()            
         except FindFailed:
             wait("1505729222650.png", 35)
             click("1505729222650.png") 
-            wait("1509617897369.png", 5)
+            wait("1509617897369.png", 15)
             click("1509617897369.png")
             wait(2)
+            accept()
