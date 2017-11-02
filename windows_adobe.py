37c516c56a4122625804e26d96461c56a3b7aa96
Gilles Pietri <gpietri@dawan.fr>
2017-11-02 12:01:15 +0100

    Premiere

--- windows_adobe.py
+++ windows_adobe.py
@@ -2,7 +2,8 @@ root = 'C:\\Program Files\\Adobe\\'
 apps = { #"After": r"Adobe After Effects CC 2015\Support Files\AfterFX.exe",
         #"Audition": r"Adobe Audition CC 2015\Adobe Audition CC.exe"
         #, "Bridge": r"Adobe Bridge CC (64 Bit)\Bridge.exe",
-        "LightRoom": r"Adobe Lightroom\lightroom.exe"
+        #"LightRoom": r"Adobe Lightroom\lightroom.exe",
+        "Premiere": r"Adobe Premiere Pro CC 2015\Adobe Premiere Pro.exe"
         }
 def accept():
     wait("1509618378336.png", 15)
