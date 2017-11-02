fe57241ae65c27144200e002702e59635397f0ad
Gilles Pietri <gpietri@dawan.fr>
2017-11-02 11:32:09 +0100

    continuer en cas d'échec pour les clics attendus

--- windows_adobe.py
+++ windows_adobe.py
@@ -3,6 +3,7 @@ apps = { #"After": r"Adobe After Effects CC 2015\Support Files\AfterFX.exe",
         "Audition": r"Adobe Audition CC 2015\Adobe Audition CC.exe"
         #, "Bridge": r"Adobe Bridge CC (64 Bit)\Bridge.exe"
         }
+setFindFailedResponse(SKIP)
 for app, path in apps.items():
         print('Considering ' + app + '(' + root + path + ')')
         sapp = App(root + path)
