ec3fe1415eb646d67b3a0f1b4d5264d7c444b1da
Gilles Pietri <gpietri@dawan.fr>
2017-11-02 11:19:11 +0100

    Limiter à Audition, lancer l'éval une fois le popup ouvert

--- windows_adobe.py
+++ windows_adobe.py
@@ -1,15 +1,18 @@
 root = 'C:\\Program Files\\Adobe\\'
-apps = { "After": r"Adobe After Effects CC 2015\Support Files\AfterFX.exe",
-        "Audition": r"Adobe Audition CC 2015\Adobe Audition CC.exe", 
-        "Bridge": r"Adobe Bridge CC (64 Bit)\Bridge.exe"
+apps = { #"After": r"Adobe After Effects CC 2015\Support Files\AfterFX.exe",
+        "Audition": r"Adobe Audition CC 2015\Adobe Audition CC.exe"
+        #, "Bridge": r"Adobe Bridge CC (64 Bit)\Bridge.exe"
         }
 for app, path in apps.items():
         print('Considering ' + app + '(' + root + path + ')')
         sapp = App(root + path)
         sapp.open()
         try:
+            wait("1505729222650.png", 20*1000)
+            click("1505729222650.png") 
+            wait("1509617897369.png")
+            click("1509617897369.png")
             wait(2)
         except FindFailed:
-            wait("1505729222650.png")
-            click("1505729222650.png")
+
             wait(2)
