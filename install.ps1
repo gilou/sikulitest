b829971f6af6c6a5850ed335372c40e846d30e3c
Gilles Pietri <gpietri@dawan.fr>
2017-11-02 12:59:27 +0100

    mot clé options pour faire du unattended

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
 Invoke-WebRequest https://launchpad.net/sikuli/sikulix/1.1.1/+download/sikulixsetup-1.1.1.jar -OutFile C:\Temp\sikulixsetup-1.1.1.jar
-java -jar "C:\Temp\sikulixsetup-1.1.1.jar" 1.1
+java -jar "C:\Temp\sikulixsetup-1.1.1.jar" options 1.1
 cd C:\Temp
 git clone https://github.com/gilou/sikulitest.git windows_adobe.sikuli
