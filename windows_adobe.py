6cf8ecdf3b3cba7a9859cca8c4bd5ddd2126cb5e
Gilles Pietri <gpietri@dawan.fr>
2017-11-03 15:39:00 +0100

    Moaaaar

--- windows_adobe.py
+++ windows_adobe.py
@@ -29,7 +29,7 @@ for app, path in sorted(apps.items()):
     setFindFailedResponse(ABORT)
     print('Considering ' + app + '(' + root + path + ')')
     sapp = App(root + path)
-    os.system("taskkill /f /t /im adobe_lic.exe")
+    os.system("taskkill /f /t /im adobe_lic*.exe*")
     if not sapp.isRunning(): sapp.open()
     print('app seen as: {} main window as: {} PID is {}'.format(
                                                 sapp.getName(),
@@ -37,12 +37,12 @@ for app, path in sorted(apps.items()):
                                                 sapp.getPID()
                                             )
     )
-    
+    print('Waiting for {} to spawn a window'.format(sapp.getName()))
     while not sapp.hasWindow():
         wait(1)
     try:
         sapp.focus()
-        accept() 
+        accept()
     except FindFailed:
         setFindFailedResponse(SKIP)
         delay = 10
